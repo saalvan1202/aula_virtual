77a0874dae3b9239b86c20d911a19e6a772a2881
dh4rio <marzchelo1@gmail.com>
2025-06-05 11:00:42 -0500

    Modificación del post en otp verify

--- resources/js/Pages/Auth/OtpVerify.vue
+++ resources/js/Pages/Auth/OtpVerify.vue
@@ -2,28 +2,40 @@
     <LayoutBlank>
         <div class="auth-wrapper auth-v2">
             <div class="row auth-inner m-0">
-                <div class="col-lg-4 d-flex align-items-center auth-bg px-2 p-lg-3">
+                <div
+                    class="col-lg-4 d-flex align-items-center auth-bg px-2 p-lg-3"
+                >
                     <div class="col-12 px-xl-2 mx-auto">
-                        <h2 class="card-title font-weight-bold mb-2">Verificar OTP</h2>
+                        <h2 class="card-title font-weight-bold mb-2">
+                            Verificar OTP
+                        </h2>
                         <form @submit.prevent="submit">
                             <div class="form-group">
                                 <label>Código OTP</label>
                                 <input
                                     v-model="form.otp_code"
                                     class="form-control"
-                                    :class="{ 'is-invalid': form.errors.otp_code }"
+                                    :class="{
+                                        'is-invalid': form.errors.otp_code,
+                                    }"
                                 />
-                                <small class="text-danger" v-if="form.errors.otp_code">
+                                <small
+                                    class="text-danger"
+                                    v-if="form.errors.otp_code"
+                                >
                                     {{ form.errors.otp_code }}
                                 </small>
                             </div>
-                            <button class="btn btn-primary btn-block" :disabled="form.processing">
+                            <button
+                                class="btn btn-primary btn-block"
+                                :disabled="form.processing"
+                            >
                                 Verificar
                             </button>
                         </form>
                     </div>
                 </div>
-                      <div class="col-lg-8 d-none d-lg-flex align-items-center p-5">
+                <div class="col-lg-8 d-none d-lg-flex align-items-center p-5">
                     <div
                         class="w-100 d-lg-flex align-items-center justify-content-center px-5"
                     >
@@ -54,7 +66,16 @@ export default {
     },
     methods: {
         submit() {
-            this.form.post("/otp-verify");
+            this.$http
+                .post(this.routeTo("otp-verify"), {
+                    otp_code: this.form.otp_code,
+                })
+                .then((response) => {
+                    this.$inertia.visit(this.routeTo("/"));
+                })
+                .catch((error) => {
+                    console.log(error.response);
+                });
         },
     },
 };
