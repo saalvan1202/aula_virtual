ce24c6526ef8c3444aaaa86c5a44498f552f134a
dh4rio <marzchelo1@gmail.com>
2025-06-05 13:52:47 -0500

    Quitando reload

--- resources/js/Pages/Auth/OtpVerify.vue
+++ resources/js/Pages/Auth/OtpVerify.vue
@@ -76,8 +76,6 @@ export default {
                 .catch((error) => {
                     console.log(error.response);
                 });
-
-            window.location.reload();
         },
     },
 };
