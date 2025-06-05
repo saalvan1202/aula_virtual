abb537c0afba1bdd4e74be505ac98f812e5b87fb
saalvan1202 <ashande75@gmail.com>
2025-06-04 23:46:23 -0500

    first commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,7 +24,10 @@ class AppServiceProvider extends ServiceProvider
      * @return void
      */
     public function boot()
-    {
+    {   
+        if (env('APP_ENV') === 'production') {
+        URL::forceScheme('https');
+    }
         Validator::extendImplicit('required_zero', function ($attribute, $value, $parameters, $validator) {
             return $value !=0;
         },'el :attribute es obligatorio');
